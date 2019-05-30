0e9d1c0703c5658e3683998fbe37fe110b7ecc36
Frandaaaa <1297571059@qq.com>
2019-05-30 21:19:32 +0800

    java

--- java-code-package/泛型/src/深入泛型/设定通配符下限.java
+++ java-code-package/泛型/src/深入泛型/设定通配符下限.java
@@ -14,11 +14,31 @@ public class 设定通配符下限 {
 		//此时发现问题：元素的类型不确定（只能肯定它是T的子类），程序只能使用T来笼统的表示各种src集合的元素类型
 	}
 	
+	//此时的dest集合元素类型必须与src集合元素的类型相同或者是其父类
+	public static <T> T copy2(Collection<? super T> dest, Collection<T> src)
+	{
+		T last = null;
+		for(T ele : src)
+		{
+			last = ele;
+			dest.add(ele);
+		}
+		return last;
+	}
+	
 	public static void main(String[] args) {
 		List<Number> in = new ArrayList<>();
 		List<Integer> li = new ArrayList<>();
 		
-		//此时调用上面的该方法就会报错，因为嘛
-		Integer last = copy(in,li);
+		//此时调用上面的该方法就会报错，因为嘛程序在复制集合元素的过程中，丢失了src集合元素的类型
+//		Integer last = copy(in,li);
+		System.out.println(in);
+		
+		List<Number> ln = new ArrayList<>();
+		List<Integer> ni = new ArrayList<>();
+		li.add(5);
+		//被复制的就是Integer类型yu
+		Integer last = copy2(ln,ni);
+		System.out.println(ln);
 	}
 }
